ab5f6564dc195ffe6dc09eabf8914954f5154b6f
Josh Allen <joshmrallen@gmail.com>
2021-03-09 02:28:38 +0000

    Automatically backed up by Learn

---
+++ first_ruby_file.rb
@@ -0,0 +1,37 @@
+# puts "Hello World"
+
+# print "hi"
+
+# puts "Edvard\nMunch"
+
+# p "Hello\nWorld"
+
+=begin print "Hello\nWorld"
+
+run_code_inside = FALSE
+
+more_code_inside = FALSE
+=end
+
+=begin 
+puts "What year is it?"
+this_year = 2019
+puts "Hey, it's 2019" if this_year == 2019
+
+when do we use 'unless'? it doesn't work here.
+
+=end
+
+
+magic_exit_number = 7
+
+count = 0
+
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!"
+  count += 1
+end
+
+
+
